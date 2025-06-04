e5edac057a8d745e0530dd3ce669abc53d9025be
MaxJokar <mmb2020max@gmail.com>
2025-06-04 14:46:55 +0200

    variables sensiitive data added

--- variables.tf
+++ variables.tf
@@ -11,3 +11,22 @@ variable "region" {
   default     = "****-west-3"
   type        = string
 }
+
+
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Username for the database"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Password for the database"
+  type        = string
+  sensitive   = true
+}
+
+
